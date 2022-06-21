61002c72a7ce6226cd1c0742494949eeeef8370e
Adam Maulana <maulanadam01@gmail.com>
2022-06-21 10:32:16 +0700

    UPDATING TESTING.py

--- TESTING.py
+++ TESTING.py
@@ -5,20 +5,36 @@ def curse():
     print(f'Sn anda adalah {sn.get() or "empty"}')
 
 root = tk.Tk()
-root.geometry('600x400')
+
+one_frame = ttk.Frame()
+one_frame.pack(side='top',fill='both')
+
+two_frame = ttk.Frame()
+two_frame.pack(side='top',fill='both')
+
+
+root.geometry('1000x150')
 
 sn = tk.StringVar()
-
-sn_label = ttk.Label(root, text='Masukkan SN: ')
-sn_entry = ttk.Entry(root,textvariable=sn )
+fsp = tk.StringVar()
 
 
-top_button = ttk.Button(root,text='click me',command=curse)
-lower_button = ttk.Button(root,text='Quit',command=root.destroy)
+#INPUT SN
+sn_label = ttk.Label(one_frame, text='Masukkan SN: ')
+sn_entry = ttk.Entry(one_frame,textvariable=sn )
 
-sn_label.pack(side='left',expand=True)
-sn_entry.pack(side='left',expand=True)
-top_button.pack(side='top',expand=True)
-lower_button.pack(side='bottom',expand=True)
+#INPUT FSP
+# fsp_label = ttk.Label(left_frame, text='Masukkan FSP: ')
+# fsp_entry = ttk.Entry(right_frame,textvariable=fsp)
+
+#INPUT BUTTON
+top_button = ttk.Button(two_frame,text='click me',command=curse)
+lower_button = ttk.Button(two_frame,text='Quit',command=root.destroy)
+
+sn_label.pack(side='left')
+sn_entry.pack(side='left',expand=True,fill="x")
+top_button.pack(side='left',expand=True,fill='both')
+lower_button.pack(side='left',expand=True,fill='both')
+
 
 root.mainloop()
